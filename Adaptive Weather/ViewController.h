7dacfe61be54a4ae9ef87de4b72e6333225bae70
zac1st1k <zacfirst@gmail.com>
2015-01-13 11:45:32 +1100

    Initial Commit

---
+++ Adaptive Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adaptive Weather
+//
+//  Created by Zac on 13/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Adaptive Weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Adaptive Weather
+//
+//  Created by Zac on 13/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
